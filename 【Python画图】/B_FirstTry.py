4d8002c41b564de5f5550a043935f1e39b096998
WJ-Lai <418320384@qq.com>
2018-07-29 23:11:21 +0800

    Signed-off-by: WJ-Lai <418320384@qq.com> 实现画图

--- 【Python画图】/B_FirstTry.py
+++ 【Python画图】/B_FirstTry.py
@@ -7,6 +7,16 @@ b = 'CS'
 c = 'OVA'
 d = 'SP'
 e = 'HF'
+nodes_size = 1200
+edges_multiple = 50
+data_edges_list = [[(a, b)],[(a, c)],[(a, d)],[(a ,e)],
+                   [(b, c)],[(b, d)],[(b, e)],
+                   [(c, d)],[(c ,e)],
+                   [(d, e)]]
+data_edges_color = ['white','gray','gray','gray','darkblue','white','gray','darkblue','white','blue']
+data_edges_width = [10,10,10,10,10,10,10,10,10,10]
+
+
 #设置点的名称
 G.add_nodes_from([a, b, c, d, e])
 
@@ -21,26 +31,28 @@ red = [a, b, c]
 blue = [d]
 yello = [e]
 
-# 设置连线的颜色
-red_edges = [(a, c)]
-black_edges = [(d, b)]
-green_edges = [(a, b)]
-
+#设置连线的颜色
+#存放正相关
+steelblue_edges = [(d, b),(a, b)]
+#存放负相关
+gray_edges = [(a, c)]
 
 #设置原点的编排方式
 pos = nx.circular_layout(G)
 #设置点的参数
-nx.draw_networkx_nodes(G, pos,nodelist =red, node_color = 'r', node_size = 900)
-nx.draw_networkx_nodes(G, pos,nodelist =blue, node_color = 'b', node_size = 900)
-nx.draw_networkx_nodes(G, pos,nodelist =yello, node_color = 'y', node_size = 900)
+nx.draw_networkx_nodes(G, pos,nodelist =red, node_color = 'firebrick', node_size = nodes_size)
+nx.draw_networkx_nodes(G, pos,nodelist =blue, node_color = 'royalblue', node_size = nodes_size)
+nx.draw_networkx_nodes(G, pos,nodelist =yello, node_color = 'gold', node_size = nodes_size)
 
 #显示点的名称
-nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_labels(G, pos,font_color ='white')
 
 #设置连线的方式
-nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r',width = 10)
-nx.draw_networkx_edges(G, pos, edgelist=black_edges, edge_color='b',width = 5)
-nx.draw_networkx_edges(G, pos, edgelist=green_edges, edge_color='g',width = 10)
+for index in range(len(data_edges_color)):
+   nx.draw_networkx_edges(G, pos, edgelist=data_edges_list[index],
+                          edge_color=data_edges_color[index],
+                          width = data_edges_width[index])
+
 
 plt.axis('off')
 plt.show()
